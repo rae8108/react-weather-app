96deb6a6cca213e3f2c268b271eeaf90589e7094
rae8108 <146367231+rae8108@users.noreply.github.com>
2024-01-10 20:08:53 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        This project was created by Rayan and is{" "}
-        <a
-          href="https://github.com/rae8108/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+        <footer>
+          This project was created by Rayan and is{" "}
+          <a
+            href="https://github.com/rae8108/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
