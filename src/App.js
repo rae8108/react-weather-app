6a5624a1f4a936e3b6387f6bc2315f961d9521c6
rae8108 <146367231+rae8108@users.noreply.github.com>
2024-01-10 16:53:19 +0000

    added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        This project was created by Rayan and is
+        <a
+          href="https://github.com/rae8108/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
