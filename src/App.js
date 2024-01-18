fd651bd7b91a4843940fe16cb58caab87123aa28
rae8108 <146367231+rae8108@users.noreply.github.com>
2024-01-18 17:37:42 +0000

    created weather component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-
-        <footer>
+        <Weather />
+        <footer className="text-center">
           This project was created by Rayan and is{" "}
           <a
             href="https://github.com/rae8108/react-weather-app"
